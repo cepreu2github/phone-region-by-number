44133f8afe7cd2bbb0cb24655f8a020a55b83ebb
Sergey Variukhin <cepreu.mail@gmail.com>
2015-09-03 23:23:58 +1000

    working on stub REST Api

--- src/main/java/prbn/MainController.java
+++ src/main/java/prbn/MainController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class MainController {
-    @RequestMapping("/greet")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    //GET - вернет время последнего обновления базы, POST - выполнит обновление
+    @RequestMapping("/update/date")
+    public String getUpdateDatetime() {
+        return "31.12.2015";
     }
+    //POST - отправит номера в CSV и вернет результат обработки
+    @RequestMapping("/numbers")
+    public String getRegionsForNumbers()
+    {
+        return "31.12.2015";
+    }
+    //POST - проверка одного номера
+    @RequestMapping("/number")
+    public String getRegionForNumber()
+    {
+        return "31.12.2015";
+    }
+
+
 }
