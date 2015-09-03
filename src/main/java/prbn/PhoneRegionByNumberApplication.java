e93904724b6851c396c2b96c2856ebe729a5874c
Sergey Varyukhin <cepreu.mail@gmail.com>
2015-09-03 16:51:55 +1000

    empty project

---
+++ src/main/java/prbn/PhoneRegionByNumberApplication.java
@@ -0,0 +1,12 @@
+package prbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneRegionByNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneRegionByNumberApplication.class, args);
+    }
+}
